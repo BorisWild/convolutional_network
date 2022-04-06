6dd05f1ad2ce59073dfc3229d0f1d7ec8b582d87
BorisWild <boriswild@gmail.com>
2022-04-06 20:05:28 +0300

    initial

--- js/main.js
+++ js/main.js
@@ -1,147 +1,193 @@
-const status = document.getElementById('status');
-if (status) {
-  status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
-}
-//Note that the data is already in normalized form.
-// Since grayscale images can only contain numbers between 0 and 255,
-// dividing each value by 255 will normalize the data between 0 and 1 if you had to do this manually.
-import {TRAINING_DATA} from './mnist.js';
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
 
+import {TRAINING_DATA} from './fashion-mnist.js';
 
 // Grab a reference to the MNIST input values (pixel data).
 const INPUTS = TRAINING_DATA.inputs;
+
 // Grab reference to the MNIST output values.
 const OUTPUTS = TRAINING_DATA.outputs;
 
-// Shuffle the two arrays in the same way so inputs still match outputs indexes.
+// Shuffle the two arrays to remove any order, but do so in the same way so
+// inputs still match outputs indexes.
 tf.util.shuffleCombo(INPUTS, OUTPUTS);
 
-// The input tensor is a 2-Dimensional Tensor since it is an array of grayscale images,
-// each of which is an array of 784 values. ( [ [1, 2],[1, 3],...[...] ] )
-const INPUTS_TENSOR = tf.tensor2d(INPUTS);
+
+// Function to take a Tensor and normalize values
+// with respect to each column of values contained in that Tensor.
+function normalize(tensor, min, max) {
+  const result = tf.tidy(function() {
+    //Since the pixels of images in the Fashion MNIST dataset are always in the range of 0 to 255,
+    // the minimum and maximum values (0 and 255) can directly be passed as numbers instead of tensors
+    const MIN_VALUES = tf.scalar(min);
+    const MAX_VALUES = tf.scalar(max);
+
+    // Now calculate subtract the MIN_VALUE from every value in the Tensor
+    // And store the results in a new Tensor.
+    const TENSOR_SUBTRACT_MIN_VALUE = tf.sub(tensor, MIN_VALUES);
+
+    // Calculate the range size of possible values.
+    const RANGE_SIZE = tf.sub(MAX_VALUES, MIN_VALUES);
+
+    // Calculate the adjusted values divided by the range size as a new Tensor.
+    const NORMALIZED_VALUES = tf.div(TENSOR_SUBTRACT_MIN_VALUE, RANGE_SIZE);
+
+    // Return the important tensors.
+    return NORMALIZED_VALUES;
+  });
+  return result;
+}
+
+
+// Input feature Array is 2 dimensional.
+const INPUTS_TENSOR = normalize(tf.tensor2d(INPUTS), 0, 255);
 
 // Output feature Array is 1 dimensional.
-//The ‘tf.oneHot’ function takes two parameters,
-// the tensor that needs to be encoded and the number of classes that represent the data.
 const OUTPUTS_TENSOR = tf.oneHot(tf.tensor1d(OUTPUTS, 'int32'), 10);
 
+
+
 // Now actually create and define model architecture.
-//define model... activate layer by layer...first output will be input for another layer
 const model = tf.sequential();
-
-//Add the input layer. Since the input images are 28x28 pixels in size, the input shape is [784]
-//Set the number of neurons to 32 and add the ‘reLU’ activation function.
-//Note that the number of neurons is a parameter that an ML engineer arrives at
-// after experimentation keeping in mind the balance between accuracy and speed
+/*
 model.add(tf.layers.dense({inputShape: [784], units: 32, activation: 'relu'}));
-
-//Add a hidden layer with 16 neurons with the ‘relu’ activation function
 model.add(tf.layers.dense({units: 16, activation: 'relu'}));
+model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+*/
+model.add(tf.layers.conv2d({
+  // The input shape should be in the form of width, height, and the number of color channels.
+  // The input images are 28x28 pixels in size and grayscale, so the expected input shape is [28, 28, 1].
+  // Note: For RGB images, the last value would be three.
+  inputShape: [28, 28, 1],
+  filters: 16, // Set the number of filters to 16.
+  kernelSize: 3, // Square Filter of 3 by 3. Could also specify rectangle eg [2, 3].
+  strides: 1, //This means a filter value is calculated for every pixel in the input image.
+  padding: 'same', // This ensures missing values are filled with zeroes.
+  activation: 'relu'
+}));
 
-//Add the final output layer with 10 neurons and the ‘softmax’ activation function.
-//This ensures all outputs add up to one, essentially providing percentage confidences for each output.
+//Note that this results in an output that is 14x14 pixels in size.
+model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+
+
+model.add(tf.layers.conv2d({
+  filters: 32,
+  kernelSize: 3,
+  strides: 1,
+  padding: 'same',
+  activation: 'relu'
+}));
+
+model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+
+//Add code to run the outputs through a regular multi-layer perceptron.
+model.add(tf.layers.flatten()); // Use ‘tf.layers.flatten’ to convert the output into a list of numbers.
+model.add(tf.layers.dense({units: 128, activation: 'relu'}));
 model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
 
-//Observe that the model has around 26,000 trainable parameters.
-//Working with pixel data can lead to very large models.
+// With over 200 thousand trainable parameters, this will take a while to train
 model.summary();
 
 train();
 
-const PREDICTION_ELEMENT = document.getElementById('prediction');
-const CANVAS = document.getElementById('canvas');
-const CTX = CANVAS.getContext('2d');
 
-//TensorFlow.js also has a tf.browser.toPixels() function that you can use if you wish to stay completely in Tensor land. A very useful function if you have all the values in a Tensor of the right shape for a given image and you want to push those values to a HTML canvas in one line of code.
-function drawImage(digit) {
-  digit = tf.tensor(digit, [28, 28]);
-  tf.browser.toPixels(digit, CANVAS);
-  // Perform a new classification after a certain interval.
-  setTimeout(evaluate, 2000);
+async function train() {
+  // Compile the model with the defined optimizer and specify a loss function to use.
+  model.compile({
+    optimizer: 'adam', // Adam changes the learning rate over time which is useful.
+    loss: 'categoricalCrossentropy', // As this is a classification problem, dont use MSE.
+    metrics: ['accuracy']  // As this is a classifcation problem you can ask to record accuracy in the logs too!
+  });
+
+  // Reshape the current training data into a form that the CNN input layer can digest.
+  // Currently the training data is an array of arrays containing a list of 784 numbers.
+  // Use the ‘.reshape’ function on the input tensor with the following parameters:
+  // Size: Use ‘inputs.length’ to specify training data size.
+  // Height and Width: This should be 28x28 according to the size of the images.
+  // Channel Count: This should be one since the images are in grayscale.
+  const RESHAPED_INPUTS = INPUTS_TENSOR.reshape([INPUTS.length, 28, 28, 1]);
+
+  let results = await model.fit(RESHAPED_INPUTS, OUTPUTS_TENSOR, {
+    shuffle: true,        // Ensure data is shuffled again before using each time.
+    validationSplit: 0.15,
+    //Note that these values is arrived at after experimentation.
+    epochs: 30,           // Go over the data 30 times!
+    batchSize: 256,
+    callbacks: {onEpochEnd: logProgress}
+  });
+
+  RESHAPED_INPUTS.dispose();
+  OUTPUTS_TENSOR.dispose();
+  INPUTS_TENSOR.dispose();
+  evaluate();
 }
 
-/*function drawImage(digit) {
-  //you can access the current canvas data using CTX.getImageData.
-  //You can pass the starting position of the canvas to this function,
-  //which is 0,0 (the top left corner of the canvas).
-  var imageData = CTX.getImageData(0, 0, 28, 28);
 
-  //Here you set the Red Green and Blue channels to simply be the input images value multiplied by 255.
-  //Remember your input images were in normalized form for training the model,
-  //so to get them into regular color values you need to multiply by 255.
-  for (let i = 0; i < digit.length; i++) {
-    imageData.data[i * 4] = digit[i] * 255;      // Red Channel.
-    imageData.data[i * 4 + 1] = digit[i] * 255;  // Green Channel.
-    imageData.data[i * 4 + 2] = digit[i] * 255;  // Blue Channel.
-    imageData.data[i * 4 + 3] = 255;             // Alpha Channel.
-  }
+function logProgress(epoch, logs) {
+  console.log('Data for epoch ' + epoch, logs);
+}
 
-  // Render the updated array of data to the canvas itself.
-  CTX.putImageData(imageData, 0, 0);
 
-  // Perform a new classification after a certain interval.
-  setTimeout(evaluate, 2000);
-}*/
+const PREDICTION_ELEMENT = document.getElementById('prediction');
+
+// Map output index to label.
+const LOOKUP = ['T-shirt', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot'];
+
 
 function evaluate() {
-  const OFFSET = Math.floor((Math.random() * INPUTS.length)); // Select random from all example inputs.
+  // Select a random index from all the example images we have in the training data arrays.
+  const OFFSET = Math.floor((Math.random() * INPUTS.length));
 
-  let answer = tf.tidy(function () {
-    //Use the ‘model.predict’ function to perform a prediction on your new input.
-    //Remember, the ‘model.predict’ function expects a batch of images as input.
-    //To avoid an error, call expandDims on the tensor 1d you just created
-    //to change it to a tensor2 with just one value to avoid errors
-    let newInput = tf.tensor1d(INPUTS[OFFSET]).expandDims();
-    let output = model.predict(newInput);
+  // Clean up created tensors automatically.
+  let answer = tf.tidy(function() {
+    let newInput = normalize(tf.tensor1d(INPUTS[OFFSET]), 0, 255);
 
-    //classes
+    let output = model.predict(newInput.reshape([1, 28, 28, 1]));
     output.print();
-    //Before you use argMax, you need to use the ‘squeeze’ function first
-    //to convert the 2d output tensor into a 1d tensor
+
     return output.squeeze().argMax();
-  })
+  });
 
   answer.array().then(function(index) {
-    PREDICTION_ELEMENT.innerText = index;
+    PREDICTION_ELEMENT.innerText = LOOKUP[index];
     PREDICTION_ELEMENT.setAttribute('class', (index === OUTPUTS[OFFSET]) ? 'correct' : 'wrong');
     answer.dispose();
     drawImage(INPUTS[OFFSET]);
   });
-
 }
 
-async function train() {
-  // Compile the model with the defined optimizer and specify our loss function to use.
-  model.compile({
-    //You can use the stochastic gradient descent algorithm instead of ‘adam’,
-    //but adam has the added advantage where
-    //it automatically changes the learning rate over time for you
-    //often finding slightly better values for weights and biases
-    //thus reducing the loss further and well suited for image problems like this.
-    optimizer: 'adam',
-    loss: 'categoricalCrossentropy', //categoricalCrossentropy was used instead of meanSquaredError
-    //This contains an array of extra metrics that can be logged.
-    //In this case, log the metric ‘accuracy’ as a measure of how many images
-    // are predicted correctly from the training data.
-    metrics: ['accuracy']
-  });
+const CANVAS = document.getElementById('canvas');
 
-  let results = await model.fit(INPUTS_TENSOR, OUTPUTS_TENSOR, {
-    shuffle: true,        // Ensure data is shuffled again before using each epoch.
-    validationSplit: 0.2, //Set validationSplit to 0.2 (20%).
-    //Note that you can experiment with batch, epochs values to get the best speed and accuracy!
-    batchSize: 512,       // Update weights after every 512 examples.
-    epochs: 50,           // Go over the data 50 times!
-    callbacks: {onEpochEnd: logProgress}
-  });
+function drawImage(digit) {
+  digit = tf.tensor(digit, [28, 28]).div(255);
+  tf.browser.toPixels(digit, CANVAS);
 
-  //clear
-  OUTPUTS_TENSOR.dispose();
-  INPUTS_TENSOR.dispose();
-
-  evaluate(); // Once trained we can evaluate the model.
+  // Perform a new classification after a certain interval.
+  setTimeout(evaluate, interval);
 }
 
-function logProgress(epoch, logs){
-  console.log('Data for epoch '+epoch, Math.sqrt(logs.loss), logs.acc)
-}
+
+var interval = 2000;
+const RANGER = document.getElementById('ranger');
+const DOM_SPEED = document.getElementById('domSpeed');
+
+// When user drags slider update interval.
+RANGER.addEventListener('input', function(e) {
+  interval = this.value;
+  DOM_SPEED.innerText = 'Change speed of classification! Currently: ' + interval + 'ms';
+});
